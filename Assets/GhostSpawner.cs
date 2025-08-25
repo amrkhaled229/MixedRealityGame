4d2bc008dd39ca3d872c0a0d3fe8166ab688f7c7
amrkhaled229 <amrkhaledosman2003@gmail.com>
2025-08-25 13:42:07 +0300

    Initial commit

---
+++ Assets/GhostSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Meta.XR.MRUtilityKit; 
+
+public class GhostSpawner : MonoBehaviour
+{
+    public float spawnTimer = 1;
+    public GameObject prefabToSpawn;
+    public float minEdgeDistance = 0.3F;
+    public MRUKAnchor.SceneLabels spawnLabels;
+    public float normalOffset;
+    private float timer;
+    public int countTries = 1000;
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        if (MRUK.Instance == null || !MRUK.Instance.IsInitialized)
+            return;
+
+        timer += Time.deltaTime;
+        if (timer > spawnTimer)
+        {
+            SpawnGhost();
+            timer -= spawnTimer;
+        }
+    }
+
+    public void SpawnGhost()
+    {
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+
+
+        int currentTry = 0;
+        while (currentTry < countTries)
+        {
+            bool hasFoundPosition = room.GenerateRandomPositionOnSurface(
+                 MRUK.SurfaceType.VERTICAL,
+                 minEdgeDistance,
+                 LabelFilter.Included(spawnLabels),
+                 out Vector3 pos,
+                 out Vector3 norm
+             );
+            if (hasFoundPosition)
+            {
+                Vector3 randomPositionNormalOffset = pos + norm * normalOffset;
+                randomPositionNormalOffset.y = 0;
+                Instantiate(prefabToSpawn, randomPositionNormalOffset, Quaternion.identity);
+            }
+        }
+
+          
+    }
+}
